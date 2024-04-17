7cd6927bb94e0cf43a8a806ae7c81f91a225a4b5
Łukasz Borkowski <krawczyk0606@gmail.com>
2024-04-17 18:29:14 +0200

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-        bodyParser: {
-            sizeLimit: '1024mb' // Set desired value here
-        }
-    },
   images: {
     remotePatterns: [
       {protocol:'https', hostname:'**'}
